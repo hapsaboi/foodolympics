abd103665f7b48e9f2eb2548181b931189774cff
unknown <hapsahanis.hh@gmail.com>
2022-10-10 23:27:11 -0700

    intial commit

--- server/index.js
+++ server/index.js
@@ -42,8 +42,7 @@ io.on("connection", (socket) => {
 	console.log("connected: " + socket.id)
 
 	app.use('/api/ticket/update_ticket', async (req, res) => {
-		console.log(req.body || "undefined");
-		const { ticket_ref, status, reference, amount } = JSON.parse(req.body);
+		const { ticket_ref, status, reference, amount } = req.body;
 		console.log("sent the request");
 		if (!ticket_ref || !status || !reference || !amount) {
 			return res.status(400).send({ msg: 'some fields not present', status: false });
