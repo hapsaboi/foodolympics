8cfed398c6781cf8478a4d7bf2560643ba5902c1
unknown <hapsahanis.hh@gmail.com>
2022-10-10 23:12:33 -0700

    intial commit

--- server/index.js
+++ server/index.js
@@ -42,38 +42,41 @@ io.on("connection", (socket) => {
 	console.log("connected: " + socket.id)
 
 	app.use('/api/ticket/update_ticket', async (req, res) => {
-		const { ticket_ref, status, reference, amount } = req.body;
-		console.log(req.body)
+		const { ticket_ref, status, reference, amount } = req.data;
+		console.log(req.body);
+
+		console.log(req.data);
+
 		console.log("sent the request");
-		// if (!ticket_ref || !status || !reference || !amount) {
-		// 	return res.status(400).send({ msg: 'some fields not present', status: false });
-		// }
-		// else {
-
-		try {
-			let found = await Ticket.findOne({ ticket_ref });
-
-			//if ticket found
-			if (!found) return res.status(400).send({ status: false, msg: 'Error finding ticket' });
-			else {
-				found.status = status;
-				found.payment_details = { ...found.payment_details, reference, paid_amount: amount };
-				found.save();
-			}
-
-			socket.to(found.socket_id).emit("msg", { ticket_ref, status, reference, amount });
-			// let data_sent = { ref_num: ticket_ref, amount, email: found.email };
-			// await axios.post('https://zingerad.zingerwallet.com/api/v1/send-ticket-email/', data_sent);
-
-			return res
-				.status(200)
-				.send({ status: true, msg: "Payment processed successfully" });
-
-		} catch (err) {
-			console.log(err);
-			res.status(500).json({ msg: err, status: false });
+		if (!ticket_ref || !status || !reference || !amount) {
+			return res.status(400).send({ msg: 'some fields not present', status: false });
 		}
-		// }
+		else {
+
+			try {
+				let found = await Ticket.findOne({ ticket_ref });
+
+				//if ticket found
+				if (!found) return res.status(400).send({ status: false, msg: 'Error finding ticket' });
+				else {
+					found.status = status;
+					found.payment_details = { ...found.payment_details, reference, paid_amount: amount };
+					found.save();
+				}
+
+				socket.to(found.socket_id).emit("msg", { ticket_ref, status, reference, amount });
+				// let data_sent = { ref_num: ticket_ref, amount, email: found.email };
+				// await axios.post('https://zingerad.zingerwallet.com/api/v1/send-ticket-email/', data_sent);
+
+				return res
+					.status(200)
+					.send({ status: true, msg: "Payment processed successfully" });
+
+			} catch (err) {
+				console.log(err);
+				res.status(500).json({ msg: err, status: false });
+			}
+		}
 
 
 	});
