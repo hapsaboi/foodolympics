bc307d07e339c2a2fce45171b759f1e7553f952d
unknown <hapsahanis.hh@gmail.com>
2022-10-11 21:18:09 -0700

    first commit

--- client/src/views/admin/views/Dashboard.js
+++ client/src/views/admin/views/Dashboard.js
@@ -8,7 +8,6 @@ import {
   Row,
   Col,
 } from "react-bootstrap";
-import { useAuth } from "contexts/AuthContext";
 import Notifications from "components/Notification/Notification";
 
 
@@ -25,7 +24,7 @@ function Dashboard() {
         await axios.get(ticket.showStats).then((res) => {
 
           if (res.data.status === true) {
-            setStats(res.data);
+            setStats(res.data.data);
           }
           else {
             setNotificationDetails({ msg: "Error Loading Dashoard, Please Referesh The Page", type: "danger" });
@@ -44,119 +43,124 @@ function Dashboard() {
       {notificationStatus === true ? <Notifications details={notificationDetails} /> : null}
 
       <Container fluid>
+        {Object.keys(stats).length > 0 ?
+          <>
+            <Row>
+              <Col lg="6" sm="6">
+                <Card className="card-stats">
+                  <Card.Body>
+                    <Row>
+                      <Col xs="5">
+                        <div className="icon-big text-center icon-warning">
+                          <i className="nc-icon nc-circle-09 text-warning"></i>
+                        </div>
+                      </Col>
+                      <Col xs="7">
+                        <div className="numbers">
+                          <p className="card-category">Succesful Tickets</p>
+                          <Card.Title as="h4">Succesful Tickets: {stats?.success}</Card.Title>
+                        </div>
+                      </Col>
+                    </Row>
+                  </Card.Body>
+                  <Card.Footer>
+                    <hr></hr>
+                    <div className="stats">
+                      <i className="fas fa-redo mr-1"></i>
+                      Update Now
+                    </div>
+                  </Card.Footer>
+                </Card>
+              </Col>
 
-        <Row>
-          <Col lg="6" sm="6">
-            <Card className="card-stats">
-              <Card.Body>
-                <Row>
-                  <Col xs="5">
-                    <div className="icon-big text-center icon-warning">
-                      <i className="nc-icon nc-circle-09 text-warning"></i>
+              <Col lg="6" sm="6">
+                <Card className="card-stats">
+                  <Card.Body>
+                    <Row>
+                      <Col xs="5">
+                        <div className="icon-big text-center icon-warning">
+                          <i className="nc-icon nc-circle-09 text-warning"></i>
+                        </div>
+                      </Col>
+                      <Col xs="7">
+                        <div className="numbers">
+                          <p className="card-category">Units Sold</p>
+                          <Card.Title as="h4">Units Sold: {stats?.units[0]?.sum}</Card.Title>
+                        </div>
+                      </Col>
+                    </Row>
+                  </Card.Body>
+                  <Card.Footer>
+                    <hr></hr>
+                    <div className="stats">
+                      <i className="fas fa-redo mr-1"></i>
+                      Update Now
                     </div>
-                  </Col>
-                  <Col xs="7">
-                    <div className="numbers">
-                      <p className="card-category">Succesful Tickets</p>
-                      <Card.Title as="h4">Succesful Tickets: {stats?.success}</Card.Title>
-                    </div>
-                  </Col>
-                </Row>
-              </Card.Body>
-              <Card.Footer>
-                <hr></hr>
-                <div className="stats">
-                  <i className="fas fa-redo mr-1"></i>
-                  Update Now
-                </div>
-              </Card.Footer>
-            </Card>
-          </Col>
+                  </Card.Footer>
+                </Card>
+              </Col>
+            </Row>
 
-          <Col lg="6" sm="6">
-            <Card className="card-stats">
-              <Card.Body>
-                <Row>
-                  <Col xs="5">
-                    <div className="icon-big text-center icon-warning">
-                      <i className="nc-icon nc-circle-09 text-warning"></i>
+            <Row>
+              <Col lg="6" sm="6">
+                <Card className="card-stats">
+                  <Card.Body>
+                    <Row>
+                      <Col xs="5">
+                        <div className="icon-big text-center icon-warning">
+                          <i className="nc-icon nc-circle-09 text-warning"></i>
+                        </div>
+                      </Col>
+                      <Col xs="7">
+                        <div className="numbers">
+                          <p className="card-category">Reversed Payments</p>
+                          <Card.Title as="h4">Reversed Tickets: {stats?.reversed}</Card.Title>
+                        </div>
+                      </Col>
+                    </Row>
+                  </Card.Body>
+                  <Card.Footer>
+                    <hr></hr>
+                    <div className="stats">
+                      <i className="fas fa-redo mr-1"></i>
+                      Update Now
                     </div>
-                  </Col>
-                  <Col xs="7">
-                    <div className="numbers">
-                      <p className="card-category">Units Sold</p>
-                      <Card.Title as="h4">Units Sold: {stats?.units?.sum}</Card.Title>
-                    </div>
-                  </Col>
-                </Row>
-              </Card.Body>
-              <Card.Footer>
-                <hr></hr>
-                <div className="stats">
-                  <i className="fas fa-redo mr-1"></i>
-                  Update Now
-                </div>
-              </Card.Footer>
-            </Card>
-          </Col>
-        </Row>
+                  </Card.Footer>
+                </Card>
+              </Col>
 
-        <Row>
-          <Col lg="6" sm="6">
-            <Card className="card-stats">
-              <Card.Body>
-                <Row>
-                  <Col xs="5">
-                    <div className="icon-big text-center icon-warning">
-                      <i className="nc-icon nc-circle-09 text-warning"></i>
+              <Col lg="6" sm="6">
+                <Card className="card-stats">
+                  <Card.Body>
+                    <Row>
+                      <Col xs="5">
+                        <div className="icon-big text-center icon-warning">
+                          <i className="nc-icon nc-circle-09 text-warning"></i>
+                        </div>
+                      </Col>
+                      <Col xs="7">
+                        <div className="numbers">
+                          <p className="card-category">Used Tickets</p>
+                          <Card.Title as="h4">Used Tickets: {stats?.used}</Card.Title>
+                        </div>
+                      </Col>
+                    </Row>
+                  </Card.Body>
+                  <Card.Footer>
+                    <hr></hr>
+                    <div className="stats">
+                      <i className="fas fa-redo mr-1"></i>
+                      Update Now
                     </div>
-                  </Col>
-                  <Col xs="7">
-                    <div className="numbers">
-                      <p className="card-category">Reversed Payments</p>
-                      <Card.Title as="h4">Reversed Tickets: {stats?.reversed}</Card.Title>
-                    </div>
-                  </Col>
-                </Row>
-              </Card.Body>
-              <Card.Footer>
-                <hr></hr>
-                <div className="stats">
-                  <i className="fas fa-redo mr-1"></i>
-                  Update Now
-                </div>
-              </Card.Footer>
-            </Card>
-          </Col>
+                  </Card.Footer>
+                </Card>
+              </Col>
 
-          <Col lg="6" sm="6">
-            <Card className="card-stats">
-              <Card.Body>
-                <Row>
-                  <Col xs="5">
-                    <div className="icon-big text-center icon-warning">
-                      <i className="nc-icon nc-circle-09 text-warning"></i>
-                    </div>
-                  </Col>
-                  <Col xs="7">
-                    <div className="numbers">
-                      <p className="card-category">Used Tickets</p>
-                      <Card.Title as="h4">Used Tickets: {stats?.used}</Card.Title>
-                    </div>
-                  </Col>
-                </Row>
-              </Card.Body>
-              <Card.Footer>
-                <hr></hr>
-                <div className="stats">
-                  <i className="fas fa-redo mr-1"></i>
-                  Update Now
-                </div>
-              </Card.Footer>
-            </Card>
-          </Col>
-
-        </Row>
+            </Row>
+          </>
+          :
+          null
+        }
 
       </Container >
     </>
